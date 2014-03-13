6c1c13131440965243f5dac14835b6555f44bbd8
bdemoulin <bat.demoulin@gmail.com>
2014-03-13 13:57:55 +0100

    Comments and modifications on find_energies.py

--- find_energies.py
+++ find_energies.py
@@ -3,6 +3,10 @@ import sys
 import csv
 import numpy as np
 
+file_to_open = "caspt2.output"
+
+####################################################
+
 def excitation_ev(excited,ground):
     excited = float(excited)
     ground = float(ground)
@@ -10,18 +14,23 @@ def excitation_ev(excited,ground):
     excitation_ev = 27.2105*excitation_h
     return excitation_ev
 
-caspt2_output = open("caspt2.output","r").readlines()
+####################################################
+
+caspt2_output = open(file_to_open,"r").readlines()
 
 i_root = 0
 
+# Initialization of the tables for the raw energy
 RASSCF_energy = []
 CASPT2_energy = []
 MSCASPT2_energy = []
 
+# Initialization of the tables for the computed excitation energies
 RASSCF_excitation = []
 CASPT2_excitation = []
 MSCASPT2_excitation = []
 
+# Read the file to find the corresponding energies
 for line in caspt2_output:
     if str(line).startswith("::"): 
          line = line.split()
@@ -37,22 +46,28 @@ for line in caspt2_output:
              MSCASPT2_energy += [line[6]]
 
 # Calculation of excitation energies for RASSCF
-
 for j in range(1, i_root):
-    excite_RASSCF = "%.5f" % excitation_ev(RASSCF_energy[j],RASSCF_energy[0])
+    excite_RASSCF = "%.5f" % excitation_ev(RASSCF_energy[j],
+                                           RASSCF_energy[0])
     RASSCF_excitation += [ excite_RASSCF ]
-    excite_CASPT2 = "%.5f" % excitation_ev(CASPT2_energy[j],CASPT2_energy[0])
+    excite_CASPT2 = "%.5f" % excitation_ev(CASPT2_energy[j],
+                                           CASPT2_energy[0])
     CASPT2_excitation += [ excite_CASPT2 ]
-    excite_MSCASPT2 = "%.5f" % excitation_ev(MSCASPT2_energy[j],MSCASPT2_energy[0])
+    excite_MSCASPT2 = "%.5f" % excitation_ev(MSCASPT2_energy[j],
+                                             MSCASPT2_energy[0])
     MSCASPT2_excitation += [ excite_MSCASPT2 ]
 
-excitation = np.array( [ (RASSCF_excitation), (CASPT2_excitation), (MSCASPT2_excitation) ], dtype=float )
+excitation = np.array( [ (RASSCF_excitation), 
+                         (CASPT2_excitation), 
+                         (MSCASPT2_excitation) ], 
+                         dtype=float )
+
 excitation_nm = 1240 / excitation
 
 # Write excitations in output file
-
 np.savetxt("excitations_nm.csv", excitation_nm, fmt='%.2f')
 np.savetxt("excitations.csv", excitation, fmt='%.5f')
 
+# Print the result 
 result = os.system("cat excitations.csv")
 result_nm = os.system("cat excitations_nm.csv")
